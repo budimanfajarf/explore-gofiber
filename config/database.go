895dfe45a0ac64e41b663ce4b3d703f45de642e7
Budi <budimanfajarf@gmail.com>
2023-10-09 11:43:40 +0700

    perf(db): log db query

--- config/database.go
+++ config/database.go
@@ -6,6 +6,7 @@ import (
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var (
@@ -22,7 +23,9 @@ func ConnectDb() {
 		To handle time.Time correctly, you need to include parseTime as a parameter. (more parameters)
 		To fully support UTF-8 encoding, you need to change charset=utf8 to charset=utf8mb4. See this article for a detailed explanation
 	*/
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 
 	if err != nil {
 		log.Fatal("Failed to connect to database. \n", err)
