1c5f1753edbc178ea26bbafbcdd5f8c3274956d9
Budi <budimanfajarf@gmail.com>
2023-10-18 06:55:16 +0700

    perf(model): create various base models

--- models/base.go
+++ models/base.go
@@ -7,3 +7,22 @@ type Base struct {
 	CreatedAt time.Time `gorm:"column:createdAt" json:"createdAt"`
 	UpdatedAt time.Time `gorm:"column:updatedAt" json:"updatedAt"`
 }
+
+type BaseModel struct {
+	Base
+
+	CreatedBy uint `gorm:"column:createdBy" json:"createdBy"`
+	UpdatedBy uint `gorm:"column:updatedBy" json:"updatedBy"`
+}
+
+type BaseWithSoftDelete struct {
+	Base
+
+	DeletedAt time.Time `gorm:"column:deletedAt" json:"deletedAt"`
+}
+
+type BaseModelWithSoftDelete struct {
+	BaseModel
+
+	DeletedAt time.Time `gorm:"column:deletedAt" json:"deletedAt"`
+}
