4693d1929443b64e47188085da5184dbabe06315
Budi <budimanfajarf@gmail.com>
2023-10-18 06:18:13 +0700

    feat(article): create article repository

---
+++ article/article.repository.go
@@ -0,0 +1,17 @@
+package article
+
+import (
+	"explore-gofiber/database"
+)
+
+func GetArticleListData() []GetArticleListItem {
+	// // Method 1: get all columns
+	// articles := []Article{}
+	// database.MySQL.Find(&articles)
+
+	// Method 2: get specific columns
+	articles := []GetArticleListItem{}
+	database.MySQL.Model(&Article{}).Find(&articles)
+
+	return articles
+}
--- article/article.service.go
+++ article/article.service.go
@@ -1,17 +1,5 @@
 package article
 
-import (
-	"explore-gofiber/database"
-)
-
 func GetArticleList() []GetArticleListItem {
-	// // Method 1: get all columns
-	// articles := []Article{}
-	// database.MySQL.Find(&articles)
-
-	// Method 2: get specific columns
-	articles := []GetArticleListItem{}
-	database.MySQL.Model(&Article{}).Find(&articles)
-
-	return articles
+	return GetArticleListData()
 }
