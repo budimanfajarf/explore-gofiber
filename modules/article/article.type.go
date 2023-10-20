6a28a654f662658896f0be47bf6f0d5cde929102
Budi <budimanfajarf@gmail.com>
2023-10-21 04:41:05 +0700

    refactor(article): move all struct to one file

--- modules/article/article.type.go
+++ modules/article/article.type.go
@@ -10,3 +10,18 @@ type ArticleListItem struct {
 	Status    string    `json:"status" validate:"required,oneof=UNPUBLISHED PUBLISHED"`
 	CreatedAt time.Time `gorm:"column:createdAt" json:"createdAt"`
 }
+
+type StoreArticleDto struct {
+	Title     string `validate:"required,min=5,max=150"`
+	Content   string `validate:"omitempty,html"`
+	Image     string `validate:"omitempty"`
+	Status    string `validate:"required,oneof=UNPUBLISHED PUBLISHED"`
+	CreatedBy uint
+}
+
+type GetListParams struct {
+	Page   int    `validate:"omitempty,min=1" json:"page"`
+	Limit  int    `validate:"omitempty,min=1" json:"limit"`
+	Search string `validate:"omitempty" json:"search"`
+	Status string `validate:"omitempty,oneof=UNPUBLISHED PUBLISHED" json:"status"`
+}
