f7afaa26f344e491d320be15726bea1b080de285
Budi <budimanfajarf@gmail.com>
2023-10-20 17:43:14 +0700

    refactor(http): refactor success response

--- http/response.go
+++ http/response.go
@@ -8,20 +8,22 @@ type HttpError struct {
 }
 
 type HttpResponse struct {
-	Data  interface{} `json:"data"`
-	Meta  interface{} `json:"meta"`
-	Error *HttpError  `json:"error"`
+	Data interface{} `json:"data"`
+	// Meta  map[string]interface{} `json:"meta"`
+	Meta  fiber.Map  `json:"meta"`
+	Error *HttpError `json:"error"`
 }
 
-func Success(ctx *fiber.Ctx, status int, data interface{}) error {
-	return ctx.Status(status).JSON(HttpResponse{
-		Data: data,
-	})
-}
+// func Success(ctx *fiber.Ctx, status int, data interface{}, meta ...map[string]interface{}) error {
+func Success(ctx *fiber.Ctx, status int, data interface{}, meta ...fiber.Map) error {
+	var metaData fiber.Map
 
-func SuccessWithMeta(ctx *fiber.Ctx, status int, data interface{}, meta interface{}) error {
+	if len(meta) > 0 {
+		metaData = meta[0]
+	}
+
 	return ctx.Status(status).JSON(HttpResponse{
 		Data: data,
-		Meta: meta,
+		Meta: metaData,
 	})
 }
