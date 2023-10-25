f42e8c6e0d27931408614285f6d075abca400f91
Budi <budimanfajarf@gmail.com>
2023-10-25 23:26:19 +0700

    feat(utils): create common scopes and implement it to modules

--- modules/article/article.scope.go
+++ modules/article/article.scope.go
@@ -1,20 +1,10 @@
 package article
 
 import (
-	"explore-gofiber/utils"
-	"fmt"
+	"explore-gofiber/utils/scopes"
 
 	"gorm.io/gorm"
 )
-
-func StatusScope(status string) func(db *gorm.DB) *gorm.DB {
-	return func(db *gorm.DB) *gorm.DB {
-		if status != "" {
-			return db.Where("status = ?", status)
-		}
-		return db
-	}
-}
 
 func SearchScope(search string) func(db *gorm.DB) *gorm.DB {
 	return func(db *gorm.DB) *gorm.DB {
@@ -25,39 +15,22 @@ func SearchScope(search string) func(db *gorm.DB) *gorm.DB {
 	}
 }
 
+func StatusScope(status string) func(db *gorm.DB) *gorm.DB {
+	return scopes.StatusScope(status)
+}
+
 func OrderScope(orderBy string, order string) func(db *gorm.DB) *gorm.DB {
-	return func(db *gorm.DB) *gorm.DB {
-		if orderBy != "" && order != "" {
-			return db.Order(fmt.Sprintf("%s %s", orderBy, order))
-		}
-		return db
-	}
+	return scopes.OrderScope(orderBy, order)
 }
 
 func PaginationScope(page int, limit int) func(db *gorm.DB) *gorm.DB {
-	return func(db *gorm.DB) *gorm.DB {
-		return db.Offset(utils.CalculateOffset(page, limit)).Limit(limit)
-	}
+	return scopes.PaginationScope(page, limit)
 }
 
 func SelectScope(selects []string) func(db *gorm.DB) *gorm.DB {
-	return func(db *gorm.DB) *gorm.DB {
-		if len(selects) > 0 {
-			return db.Select(selects)
-		}
-		return db
-	}
+	return scopes.SelectScope(selects)
 }
 
 func RelationsScope(relations []string) func(db *gorm.DB) *gorm.DB {
-	return func(db *gorm.DB) *gorm.DB {
-		if len(relations) > 0 {
-			for _, relation := range relations {
-				if relation != "" {
-					db.Preload(relation)
-				}
-			}
-		}
-		return db
-	}
+	return scopes.RelationsScope(relations)
 }
